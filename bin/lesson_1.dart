c60e17d039852327581c3735e78cda4cd1d76d11
alexiusbond <aleksey.bondarev@iaau.edu.kg>
2025-10-25 02:27:54 +0600

    finished lesson 2

--- bin/lesson_1.dart
+++ bin/lesson_1.dart
@@ -1,17 +1,122 @@
-// Введение, знакомство с IDE VS code 
+// Введение, знакомство с IDE VS code
 //(Форматирование кода Shift+Alt+F, Shift + Alt + ↓ — дублировать строку вниз, запуск программы)
 
 void main() {
   // Вывод информации в консоль (print)
-  // Арифметические операторы
+  print('Hello world!');
+  print('Dart is awesome!');
+  print(54);
+  print(5.45);
+
   // Комментарии
-  // Базовые типы данных
+  // Однострочный комментарий
+  /* Многострочный
+     комментарий */
+
+  // Переменные и типы данных
+  String myName = 'Jim'; // строковый тип данных
+  int myAge = 25; // целочисленный тип данных
+  double myHeight = 1.75; // тип данных с плавающей точкой
+  bool isTeacher = true; // логический тип данных
+  print(myName);
+  print(myAge);
+  print(myHeight);
+  print(isTeacher);
+  String myJob; // Создание переменной (объявление) - Значение по умолчанию null
+  // print(myJob);
+  myJob = "Dart Developer"; // Присвоение значения переменной (инициализация)
+  int mySalary = 5000;
+  print(myJob);
+  print(mySalary);
+  myJob = "Senior Dart Developer"; // Изменение значения переменной
+  mySalary = mySalary + 2000;
+  print(myJob);
+  print(mySalary);
+
+  // Регистрозависимость и уникальность имен переменных, camelCase, только буквы, цифры и _
+  String myPetName1 = 'Buddy';
+  String myPetName2 = 'Max';
+  // String my pet name = 'Buddy'; // Ошибка: пробелы в имене переменной
+  // String 1stPetName = 'Buddy'; // Ошибка: имя переменной не может начинаться с цифры
+  // String my_pet_name = 'Buddy'; // Допустимо, но не принято в стиле Dart
+  print(myPetName1);
+
   // Конкатенация и интерполяция строк
+  print('My name is ' + myName + '. My job is ' + myJob + '.');
+  print('My name is $myName. My job is $myJob. I am $myAge years old.');
+  print('I born in 2025 - $myAge.');
+  print('I born in ${2025 - myAge}. My salary is ${mySalary}USD.');
+
+  // Экранирование строк
+  print("Today I'm learning Dart programming language.");
+  print('Today I\'m learning Dart programming language.');
+  print("Today I'm learning Dart programming language.");
+
+  // Многострочные строки
+  String multiLineString = '''
+  This is a multi-line string.
+  It can span multiple lines.
+  ''';
+  print(multiLineString);
+
   // Операции со строками
-  // Особенности системы типов Dart (var, final, const, dynamic)
-  // Ввод информации из консоли (stdin)
-  int a = 10;
-  print('Hello, Dart Lesson 1!');
-  print(a * 7);
-  print('End od program.');
+  String sampleString = "  Hello Dart!  ";
+  print(sampleString.length); // Длина строки
+  print(sampleString.toUpperCase()); // Преобразование в верхний регистр
+  print(sampleString.toLowerCase()); // Преобразование в нижний регистр
+  print(sampleString.trim()); // Удаление пробелов в начале и конце
+  print(sampleString.contains("Dart")); // Проверка наличия подстроки
+  print(sampleString.replaceAll("Dart", "Flutter")); // Замена подстроки
+
+  // Преобразование типов
+  String ageString = "30";
+  int ageInt = int.parse(ageString); // Преобразование строки в целое число
+  print(ageInt + 5);
+
+  double heightDouble = 1.85;
+  String heightString = heightDouble
+      .toString(); // Преобразование числа с плавающей точкой в строку
+  print("Height: " + heightString);
+
+  /*String helloString = "Hello";
+  int helloInt = int.parse(
+    helloString,
+  ); // Ошибка: Невозможно преобразовать строку в число
+  */
+
+  int number = 9;
+  double numberDouble = number
+      .toDouble(); // Преобразование целого числа в число с плавающей точкой
+  double digit = 9.99;
+  int digitint = digit
+      .toInt(); // Преобразование числа с плавающей точкой в целое число
+  print('$number, $numberDouble, $digit, $digitint');
+
+  // Арифметические операторы
+  print(1 + 4); // Сложение
+  print(9 - 2); // Вычитание
+  print(5 * 6); // Умножение
+  print(8 / 2); // Деление
+  print(7 ~/ 3); // Целочисленное деление
+  print(7 % 3); // Остаток от деления
+
+  print(5 + 3 * 4 / 2 - 1);
+  print((5 + 3) * 4 / (2 - 1));
+
+  int x = 10;
+  print(x);
+  x += 5; // x = x + 5
+  print(x);
+  print(x++); // Постфиксный инкремент
+  print(++x); // Префиксный
+  print(x);
+
+  // Операторы сравнения
+  int y = 15;
+  print(x == y); // Равно
+  print(x != y); // Не равно
+  print(x > y); // Больше
+  print(x < y); // Меньше
+  print(x >= y); // Больше или равно
+  print(x <= y); // Меньше или равно
 }
--- bin/lesson_2.dart
+++ bin/lesson_2.dart
@@ -1,11 +1,179 @@
-// Условные выражения и операторы
-// Условные конструкции if, if-else, if - else-if - else
-// Булева алгебра (and, or, not)
-// Условная конструкция switch и тернарная условная конструкция
-// Вложенные условные конструкции 
-// Режим отладки
-// Знакомство с GIT (init, add, commit, push)
+import 'dart:io';
 
 void main() {
-  
+  // Особенности системы типов Dart (var, final, const, dynamic)
+
+  // Вывод типа данных переменной с помощью ключевого слова 'var'
+  var myVariable =
+      'Hello, Dart!'; // Тип данных определяется автоматически -> String
+  print(myVariable.toUpperCase());
+
+  var number = 42; // Тип данных определяется автоматически -> int
+  print(number + 8);
+  // number = 'Five'; // Ошибка: Нельзя присвоить значение другого типа
+
+  final String finalString = 'This is a final string.';
+  // finalString = 'Trying to change'; // Ошибка: Нельзя изменить значение final - оно константно после инициализации
+
+  final pi = 3.14; // Тип данных определяется автоматически -> double
+
+  // Предпочтительнее использовать final чем var, если значение не будет изменяться
+
+  const phrase =
+      'Wellcome to Dart course!'; // Значение константы известно на этапе компиляции, но не в runtime
+  // phrase = 'Trying to change'; // Ошибка: Нельзя изменить значение const
+  final upperPhrase = phrase
+      .toUpperCase(); // Можно использовать методы для final
+
+  const x = 1;
+  const y = 2;
+  const sum = x + y; // Можно выполнять операции с const
+
+  dynamic variable = 'I am dynamic'; // Тип данных может изменяться
+  print(variable);
+  variable = 100; // Теперь это int
+  print(variable);
+
+  // var -> значение может изменяться и тип определяется автоматически
+  // final -> значение не может изменяться после инициализации, тип можно указать
+  // const -> значение не может изменяться и должно быть известно на этапе компиляции
+  // dynamic -> значение и тип могут изменяться в процессе выполнения программы
+
+  // Условные конструкции if, if-else, if - else-if - else
+
+  var temperature = 25;
+  var isRaining = false;
+
+  if (temperature > 20) {
+    // Условие истинно
+    const parkName = 'Central Park';
+    print('The temperature is $temperature°C. Let\'s go to $parkName!');
+  }
+
+  if (isRaining) {
+    // Условие ложно
+    print('It\'s raining. Don\'t forget your umbrella!');
+  }
+
+  if (temperature > 30) {
+    // Условие ложно
+    print('Go swimming!');
+  } else {
+    // Альтернативный блок кода
+    print('Go to the Gym!');
+  }
+
+  if (temperature == 25) {
+    // Условие истинно
+    print('It\'s a perfect day for a walk!');
+  } else {
+    print('Better stay indoors!');
+  }
+
+  /* else {
+    print('Do something else!');
+  } */ // Ошибка: Нельзя использовать else без if
+
+  if (temperature > 35) {
+    print('The weather is hot!');
+  } else if (temperature > 20) {
+    // Условие истинно, выполняется этот блок, остальные игнорируются
+    print('The weather is warm!');
+  } else if (temperature > 10) {
+    print('The weather is cool!');
+  } else if (temperature > 0) {
+    print('The weather is cold!');
+  } else {
+    print('The weather is freezing!');
+  }
+
+  // Булева алгебра (and, or, not)
+  const isWeekend = false;
+  isRaining = true;
+  temperature = 12;
+
+  if (isRaining && temperature < 15) {
+    // Оператор AND - истина, если оба условия истинны
+    print('Stay home and read a book.');
+  }
+
+  if (isWeekend && isRaining) {
+    // Оператор AND - ложь, так как isWeekend = false
+    print('It\'s weekend and it\'s raining. Perfect time for a movie!');
+  }
+
+  if (isWeekend || isRaining) {
+    // Оператор OR - истина, если хотя бы одно условие истинно
+    print('Either it\'s weekend or it\'s raining. Time to relax!');
+  }
+
+  if (temperature > 20 || isWeekend) {
+    // Оператор OR - ложь, так как оба условия ложны
+    print('Let\'s go for a picnic!');
+  }
+
+  if (temperature > 15 && temperature < 25 ||
+      isWeekend ||
+      isRaining && temperature < 10) {
+    // false && true || false || true && false -> 0 * 1 + 0 + 1 * 0 -> 0 + 0 + 0 -> 0 (false)
+    print('Great day for outdoor activities!');
+  }
+
+  /* if (isWeekend) {
+  } else {
+    print('Time to work!');
+  } */
+
+  if (!isWeekend) {
+    // Оператор NOT - инвертирует значение булева выражения
+    print('Time to work!');
+  }
+
+  if (isWeekend) {
+    // условие ложно - весь блок игнорируется
+    print('Go to shopping!');
+    // вложенная условная конструкция
+    if (temperature < 10) {
+      print('Buy warm clothes!');
+    } else {
+      print('Buy some snacks!');
+    }
+  }
+
+  // Тернарная условная конструкция
+  temperature = 18;
+  // Условие ? значение_если_истина : значение_если_ложь
+  String weatherMessage = (temperature > 20)
+      ? 'It\'s warm outside.'
+      : 'It\'s cool outside.';
+  print(weatherMessage);
+
+  // Ввод информации из консоли (stdin)
+
+  print('Enter a sign: +, -, *, or /:');
+  // Запуск программы необходимо сделать из консоли, чтобы можно было ввести данные -> dart run bin/lesson_2.dart
+  final String sign = stdin.readLineSync()!;
+  const num1 = 10;
+  const num2 = 5;
+
+  // Режим отладки
+  // Условная конструкция switch
+  switch (sign) {
+    case '+':
+      print('Result: ${num1 + num2}');
+      break;
+    case '-':
+      print('Result: ${num1 - num2}');
+      break;
+    case '*':
+      print('Result: ${num1 * num2}');
+      break;
+    case '/':
+      print('Result: ${num1 / num2}');
+      break;
+    default:
+      print('Invalid sign entered.');
+  }
+
+  // Знакомство с GIT (init, add, commit, push)
 }
